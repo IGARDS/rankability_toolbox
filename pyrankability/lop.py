f940c6a8cd419eebc9aab61f6f845108b09b4692
Paul <pauleanderson@gmail.com>
2020-04-10 19:38:41 +0000

    update

--- pyrankability/lop.py
+++ pyrankability/lop.py
@@ -2,6 +2,10 @@
 import itertools
 import copy
 import multiprocessing
+import tempfile
+import os
+import shutil
+import time
 
 import numpy as np
 from gurobipy import *
@@ -9,57 +13,140 @@ from joblib import Parallel, delayed
 
 from .common import *
 
-def bilp(D):
-    n = D.shape[0]
-
-    AP = Model('lop')
-   
-    AP.setParam( 'OutputFlag', False )
-
-    x = {}
-   
-    for i in range(n):
-        for j in range(n):
-            x[i,j] = AP.addVar(lb=0,vtype=GRB.BINARY,ub=1,name="x(%s,%s)"%(i,j)) #binary
- 
-    AP.update()
-
-    for i in range(n):
-        for j in range(n):
-            if j!=i:
-                AP.addConstr(x[i,j] + x[j,i] == 1)
-    AP._x = x
-    AP._constraints = []
-   
-    AP.update()
-    AP.setObjective(quicksum(D[i,j]*x[i,j] for i in range(n) for j in range(n)),GRB.MAXIMIZE)
-    AP.update()
-   
-    I = []
-    for i in range(n):
-        for j in range(n):
-            for k in range(n):
-                if j != i and k != j and k != i:
-                    idx = (i,j,k)                    
-                    I.append((i,j,k))
-                    AP.addConstr(AP._x[idx[0],idx[1]] + AP._x[idx[1],idx[2]] + AP._x[idx[2],idx[0]] <= 2)
-                      
-    AP.update()
-    AP.optimize()
+def bilp(D_orig,max_solutions=None,num_random_restarts=0):
+    n = D_orig.shape[0]
     
-    def get_sol_x_by_x(x,n):
-        def myfunc():
-            values = []
+    temp_dir = tempfile.mkdtemp()
+    
+    Pfirst = []
+    Pfinal = []
+    objs = []
+    xs = []
+    for ix in range(num_random_restarts+1):
+        if ix > 0:
+            perm_inxs = np.random.permutation(range(D_orig.shape[0]))
+            D = D_orig[perm_inxs,:][:,perm_inxs]
+        else:
+            perm_inxs = np.arange(n)
+            D = copy.deepcopy(D_orig)
+        model_file = os.path.join(temp_dir,"model.mps")
+        if os.path.isfile(model_file):
+            AP = read(model_file)
+            x = {}
             for i in range(n):
                 for j in range(n):
-                    values.append(x[i,j].X)
-            return np.reshape(values,(n,n))
-        return myfunc
-    k=AP.objVal
-    details = {"P":[],"x": get_sol_x_by_x(x,n)()}
+                    x[i,j] = AP.getVarByName("x(%s,%s)"%(i,j))
+        else:
+            AP = Model('lop')
+
+            x = {}
+
+            for i in range(n):
+                for j in range(n):
+                    x[i,j] = AP.addVar(lb=0,vtype=GRB.BINARY,ub=1,name="x(%s,%s)"%(i,j)) #binary
+
+            AP.update()
+
+            for i in range(n):
+                for j in range(n):
+                    if j!=i:
+                        AP.addConstr(x[i,j] + x[j,i] == 1)
+                    else:
+                        AP.addConstr(x[j,i] == 0)
+
+            AP.update()
+            I = []
+            for i in range(n):
+                for j in range(n):
+                    for k in range(n):
+                        if j != i and k != j and k != i:
+                            idx = (i,j,k)                    
+                            AP.addConstr(x[idx[0],idx[1]] + x[idx[1],idx[2]] + x[idx[2],idx[0]] <= 2)
+
+            if max_solutions is not None and max_solutions > 1:
+                AP.setParam(GRB.Param.PoolSolutions, max_solutions)
+                # Limit the search space by setting a gap for the worst possible solution that will be accepted
+                AP.setParam(GRB.Param.PoolGap, .5)
+                # do a systematic search for the k-best solutions
+                AP.setParam(GRB.Param.PoolSearchMode, 2)
+            AP.update()
+            AP.write(model_file)
+        
+        tic = time.perf_counter()
+        AP.setObjective(quicksum(D[i,j]*x[i,j] for i in range(n) for j in range(n)),GRB.MAXIMIZE)
+        AP.setParam( 'OutputFlag', False )
+        AP.update()
+        toc = time.perf_counter()
+        print(f"Updating opjective in {toc - tic:0.4f} seconds")
+        
+        print('Start optimization %d'%ix)
+        tic = time.perf_counter()
+        AP.optimize()
+        toc = time.perf_counter()
+        print(f"Optimization in {toc - tic:0.4f} seconds")
+        print('End optimization %d'%ix)
+
+        def get_sol_x_by_x(x,n):
+            def myfunc():
+                values = []
+                for i in range(n):
+                    for j in range(n):
+                        values.append(int(x[i,j].X))
+                return np.reshape(values,(n,n))
+            return myfunc
+        k=AP.objVal
+
+        P = []
+        if max_solutions is not None and max_solutions > 1:
+            print('Running pool search')
+            for perm in _get_P(D,AP,get_sol_x_by_x):
+                P.append(tuple(perm_inxs[np.array(perm)]))
+        elif max_solutions is not None and max_solutions == 1:
+            sol_x = get_sol_x_by_x(x,n)()
+            r = np.sum(sol_x,axis=0)
+            ranking = np.argsort(r)
+            key = tuple([int(item) for item in ranking])
+            perm = tuple(perm_inxs[np.array(key)])
+            P.append(perm)
+            reorder = np.argsort(perm_inxs)
+            xs.append(sol_x[np.ix_(reorder,reorder)])
+        
+        if ix == 0:
+            Pfirst = P
+            xfirst = get_sol_x_by_x(x,n)()
+        
+        Pfinal.extend(P)
+        objs.append(k)
+    
+    details = {"Pfirst": Pfirst, "P":Pfinal,"x": xfirst,"objs":objs,"xs":xs}
+    
+    shutil.rmtree(temp_dir)
         
     return k,details
 
+def _get_P(D,model,get_sol_x_func):
+    # Print number of solutions stored
+    nSolutions = model.SolCount
+
+    # Print objective values of solutions and create a list of those that have the same objective value as the optimal
+    alternative_solutions = []
+    for e in range(nSolutions):
+        model.setParam(GRB.Param.SolutionNumber, e)
+        if model.PoolObjVal == model.objVal:
+            alternative_solutions.append(e)
+    
+    # get all alternative solutions
+    P = {}
+    for e in alternative_solutions:
+        model.setParam(GRB.Param.SolutionNumber, e)
+        sol_x = get_sol_x_func(model._x,D.shape[0])()
+        r = np.sum(sol_x,axis=0)
+        ranking = np.argsort(r)
+        key = tuple([int(item) for item in ranking])
+        if key not in P:
+            P[key] = True
+    P = list(P.keys())#[tuple(perm) for perm in P.keys()]
+    return P
 
 
 def lp(D,relaxation_method=None,level=2):
@@ -301,7 +388,7 @@ def objective_count(Dordered,D,Xstar,min_value,group):
     return min_value, permutations
 
 def objective_count_perm(Dorig,perm):
-    D = Dorig[perm,:][:,perm]
+    D = Dorig[np.ix_(perm,perm)]#[perm,:][:,perm]
     return np.sum(np.sum(np.triu(D)))
 
 def objective_count_exhaustive(D,X):
